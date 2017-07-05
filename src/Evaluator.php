cca1eec52ccc85c2cf8f29c3a56b1556fda0280f
José Henrique Medeiros Felipetto <jose.felipetto@pucpr.br>
2017-07-05 14:51:16 -0300

    Pull request

--- src/Evaluator.php
+++ src/Evaluator.php
@@ -2,90 +2,99 @@
 
 namespace Parser;
 
-class Evaluator {
+class Evaluator
+{
 
-	/*
-		@var $expression String Should have the expression to be evaluated.
-	*/
-	protected $expression;
+    /*
+        @var $expression String Should have the expression to be evaluated.
+    */
+    protected $expression;
 
+    /*
+        @var $ast mixed[] The abstract syntactic tree
+    */
+    protected $ast;
 
-	/*
-		@var $ast mixed[] The abstract syntactic tree
-	*/
-	protected $ast;
+    /*
+        @var $result Float Where the result will be stored
+    */
+    protected $result;
 
-	/*
-		@var $result Float Where the result will be stored
-	*/
-	protected $result;
+    /*
+        Constructor. Generate the ast and try to evaluate.
+    */
+    public function __construct()
+    {
+        // Constructor Left Blank
+    }
 
-	/*
-		Constructor. Generate the ast and try to evaluate.
-	*/
-	public function __construct(){
-			
-	}
+    /*
+        Initiate the whole process and set the result property.
 
+    */
+    public function parse(string $expression)
+    {
+        $this->expression = $expression;
+        $syntactic = new Syntactic($this->expression);
 
-	/*
-		Initiate the whole process and set the result property.
+        // try to generate the abstract syntactic tree (ast)
+        try
+        {
+            $this->ast = $syntactic->parse();
+        }catch(\RunTimeException $e){
+            echo $e->getMessage();
+        }
 
-	*/
-	public function parse(string $expression){
-		$this->expression = $expression;
-		$syntactic = new Syntactic($this->expression);
+        // try to evaluate the expression
+        try
+        {
+            $this->result = $this->evaluate($this->ast);
+        }catch(\RunTimeException $e)
+        {
+            echo $e->getMessage();
+        }
+    }
 
-		// try to generate the abstract syntactic tree (ast)
-		try{
-			$this->ast = $syntactic->parse();
-		}catch(\RunTimeException $e){
-			echo $e->getMessage();
-		}
+    /*
+        public interface to get the result
+        @return Float
+    */
+    public function getResult()
+    {
+        return $this->result;
+    }
 
-		// try to evaluate the expression
-		try{
-			$this->result = $this->evaluate($this->ast);
-		}catch(\RunTimeException $e){
-			echo $e->getMessage();
-		}
-	}
+    /*
+        Evaluate recursively the ast.
+        @return  Float
+        @throws  RunTimeException
 
-
-
-	/*
-		public interface to get the result
-		@return Float
-	*/
-	public function getResult(){
-		return $this->result;
-	}
-
-	/*
-		Evaluate recursively the ast. 
-		@return  Float
-		@throws  RunTimeException
-
-	*/
-	private function evaluate($ast){
-		if($ast['tag'] == 'Number'){
-			return $ast[0];
-		}else if($ast['tag'] == 'Plus'){
-			return $this->evaluate($ast[0]) + $this->evaluate($ast[1]);
-		}else if($ast['tag'] == 'Minus'){
-			return $this->evaluate($ast[0] - $ast[1]);
-		}else if($ast['tag'] == 'Times'){
-			return $this->evaluate($ast[0]) * $this->evaluate($ast[1]);
-		}else if($ast['tag'] == 'Division'){
-			if($ast[1] == 0)
-				throw new RunTimeException("Division by 0");
-			return $this->evaluate($ast[0]) / $this->evaluate($ast[1]);
-		}else if($ast['tag'] == 'Power'){
-			return pow($this->evaluate($ast[0]),$this->evaluate($ast[1]));
-		}else if($ast['tag'] == 'Unary'){
-			return (0 - $this->evaluate($ast[0]));
-		}
-	}
-
-	
+    */
+    private function evaluate($ast)
+    {
+        if($ast['tag'] == 'Number')
+        {
+            return $ast[0];
+        } else if($ast['tag'] == 'Plus')
+        {
+            return $this->evaluate($ast[0]) + $this->evaluate($ast[1]);
+        } else if($ast['tag'] == 'Minus')
+        {
+            return $this->evaluate($ast[0] - $ast[1]);
+        } else if($ast['tag'] == 'Times')
+        {
+            return $this->evaluate($ast[0]) * $this->evaluate($ast[1]);
+        } else if($ast['tag'] == 'Division')
+        {
+            if($ast[1] == 0)
+                throw new RunTimeException("Division by 0");
+            return $this->evaluate($ast[0]) / $this->evaluate($ast[1]);
+        } else if($ast['tag'] == 'Power')
+        {
+            return pow($this->evaluate($ast[0]),$this->evaluate($ast[1]));
+        } else if($ast['tag'] == 'Unary')
+        {
+            return (0 - $this->evaluate($ast[0]));
+        }
+    }
 }
--- src/Lexer.php
+++ src/Lexer.php
@@ -1,132 +1,136 @@
 <?php
-
 namespace Parser;
+
 
 use Helpers\Token;
 
 
 /*
-	Tokenize a numeric expression
-
-	@author José Henrique Medeiros Felipetto - jose.felipetto@pucpr.br
+    Tokenize a numeric expression
+    @author José Henrique Medeiros Felipetto - jose.felipetto@pucpr.br
 */
-class Lexer {
+class Lexer
+{
 
-	
-	/*
-		@var string $expression  Should contain the expression to be tokenized
-	*/
-	protected $expression;
 
-	/*
-		@var array $tokens       A array with token instances
-	*/
-	protected $tokens;
+    /*
+        @var string $expression  Should contain the expression to be tokenized
+    */
+    protected $expression;
 
-	
+    /*
+        @var array $tokens       A array with token instances
+    */
+    protected $tokens;
 
-	/*
-		Constructor. Try to tokenize the given expression.
-	*/
-	public function __construct($expression){
-
-		$this->expression = $expression;
-		$this->tokens     = [];
-
-		try{
-			$this->tokenize();
-		}catch(InvalidArgumentException $e){
-			echo $e->getMessage();
-		}
-
-	}
-
-	/*
-		Each time that function is called, returns the next token on the list(an instance of token)
-		@return Token
-	*/
-	public function getNextToken(){
-
-		$ret = current($this->tokens);  // get the current value of the internal pointer
-		
-		if(!$ret)
-			return (new Helpers\Token('EOF'));
-
-		$aux = next($this->tokens); // increase the internal pointer, but don't return it.
-		return $ret;
-
-	}
-
-	/*
-		Tokenize given expression and put into the tokens property
-		@throws InvalidArgumentException
-	*/
-	protected function tokenize(){
-
-		if(empty($this->expression)){
-			throw new \InvalidArgumentException("Cannot process an empty expression");
-		}
-
-		$expressionSize = strlen($this->expression);
-		
-		$i = 0;
-		$number = '';
-		$currentChar = '';
-
-		while($i < $expressionSize){
-
-			$currentChar = $this->expression[$i];
-			if($currentChar === ' ' || $currentChar === '\n' || $currentChar === '\t'){
-				$i++;
-				continue;
-			}else if(is_numeric($currentChar)){
-
-				while(is_numeric($currentChar)){
-					$number .= $currentChar;
-					$i++;
-					if($i >= $expressionSize) break;
-					$currentChar = $this->expression[$i];
-				}
-
-				// Checks if it is a flutuan point
-				if($currentChar === '.'){
-					$number .= $currentChar;
-					$i++;
-					$currentChar = $this->expression[$i];
-					if(is_numeric($currentChar)){
-						while(is_numeric($currentChar)){
-							$number .= $currentChar;
-							$i++;
-							if($i >= $expressionSize) break;
-							$currentChar = $this->expression[$i];
-						}	
-					}else{
-						throw new \InvalidArgumentException("Flutuant points shoud be like [0-9].[0-9][0-9]* ");
-					}
-				}
-				$token = new Helpers\Token('Number',(float)$number);
-				$i--;
-				$number = '';
-				
-			}else if($currentChar === '+'){
-				$token = new Helpers\Token('+');
-			}else if($currentChar === '-'){
-				$token = new Helpers\Token('-');
-			}else if($currentChar === '*'){
-				$token = new Helpers\Token('*');
-			}else if($currentChar === '/'){
-				$token = new Helpers\Token('/');
-			}else if($currentChar === '^'){
-				$token = new Helpers\Token('^');
-			}else if($currentChar === "("){
-				$token = new Helpers\Token('(');
-			}else if($currentChar === ")"){
-				$token = new Helpers\Token(')');
-			}else{
-				throw new \InvalidArgumentException("Invalid token {$currentChar} were given ");
-			}
-			$i++;
-			$this->tokens[] = $token;
-		}
-	}
+    /*
+        Constructor. Try to tokenize the given expression.
+    */
+    public function __construct($expression)
+    {
+        $this->expression = $expression;
+        $this->tokens     = [];
+        try
+        {
+            $this->tokenize();
+        }catch(InvalidArgumentException $e)
+        {
+            echo $e->getMessage();
+        }
+    }
+    /*
+        Each time that function is called, returns the next token on the list(an instance of token)
+        @return Token
+    */
+    public function getNextToken()
+    {
+        $ret = current($this->tokens);  // get the current value of the internal pointer
+        if(!$ret)
+            return (new Helpers\Token('EOF'));
+        $aux = next($this->tokens); // increase the internal pointer, but don't return it.
+        return $ret;
+    }
+    /*
+        Tokenize given expression and put into the tokens property
+        @throws InvalidArgumentException
+    */
+    protected function tokenize()
+    {
+        if(empty($this->expression))
+        {
+            throw new \InvalidArgumentException("Cannot process an empty expression");
+        }
+        $expressionSize = strlen($this->expression);
+        
+        $i = 0;
+        $number = '';
+        $currentChar = '';
+        while($i < $expressionSize)
+        {
+            $currentChar = $this->expression[$i];
+            if($currentChar === ' ' || $currentChar === '\n' || $currentChar === '\t')
+            {
+                $i++;
+                continue;
+            } else if(is_numeric($currentChar))
+            {
+                while(is_numeric($currentChar))
+                {
+                    $number .= $currentChar;
+                    $i++;
+                    if($i >= $expressionSize) break;
+                    $currentChar = $this->expression[$i];
+                }
+                // Checks if it is a flutuan point
+                if($currentChar === '.')
+                {
+                    $number .= $currentChar;
+                    $i++;
+                    $currentChar = $this->expression[$i];
+                    if(is_numeric($currentChar))
+                    {
+                        while(is_numeric($currentChar))
+                        {
+                            $number .= $currentChar;
+                            $i++;
+                            if($i >= $expressionSize) break;
+                            $currentChar = $this->expression[$i];
+                        }
+                    } else
+                    {
+                        throw new \InvalidArgumentException("Flutuant points shoud be like [0-9].[0-9][0-9]* ");
+                    }
+                }
+                $token = new Helpers\Token('Number',(float)$number);
+                $i--;
+                $number = '';
+            } else if($currentChar === '+')
+            {
+                $token = new Helpers\Token('+');
+            } else if($currentChar === '-')
+            {
+                $token = new Helpers\Token('-');
+            } else if($currentChar === '*')
+            {
+                $token = new Helpers\Token('*');
+            } else if($currentChar === '/')
+            {
+                $token = new Helpers\Token('/');
+            } else if($currentChar === '^')
+            {
+                $token = new Helpers\Token('^');
+            } else if($currentChar === "(")
+            {
+                $token = new Helpers\Token('(');
+            } else if($currentChar === ")")
+            {
+                $token = new Helpers\Token(')');
+            } else
+            {
+                throw new \InvalidArgumentException("Invalid token {$currentChar} were given ");
+            }
+            $i++;
+            $this->tokens[] = $token;
+        }
+    }
 }
